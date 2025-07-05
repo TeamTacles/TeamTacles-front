cdc617fd2c26de90dee1346f3d77fd7ee4f4030f
Gabriela Santana <gabisantanadev@gmail.com>
2025-07-05 14:44:25 -0300

    Add FormCard component
    Introduced the FormCard component to display the logo above its children. Added relevant styles for the logo and container.

--- components/FormCard.tsx
+++ components/FormCard.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+const logo = require('../assets/logo.png');
 
 interface FormCardProps {
     children: React.ReactNode;
 }
+
+function FormCard({ children }: FormCardProps) {
+    return (
+        <View style={styles.containerCard}>
+            <Image source={logo} style={styles.logo} />
+            {children}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    containerCard: {
+        margin: 8,
+        padding: 16,
+        borderRadius: 10,
+        backgroundColor: '#232323',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.2,
+        maxWidth: '80%',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 130,
+        height: 100,
+        marginBottom: 10,
+    },
+});
+
+export default FormCard;
